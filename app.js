611b9638cad81e560ae22ff35d71b71b9fbf7361
Aaron Taylor <halfnote1004@gmail.com>
2017-11-12 07:38:48 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 var link = "https://api.tripadvisor.com/api/partner/2.0/location/155507?key=e8fe9217-3f08-4ed1-8edf-87adc35c424f"
 function getData() {
-  // locationdata = $.getJSON(link).done(function(data) {
-  //     return data;
-  //   });
-  // document.getElementById("data").innerHTML = locationdata;
-  
-  function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-    var response = JSON.parse(response);
-    document.getElementById("data").innerHTML = response;
-  });
+  locationdata = $.getJSON(link).done(function(data) {
+      return data;
+    });
+  document.getElementById("data").innerHTML = locationdata;
 }
-}
+
 
